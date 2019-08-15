604b27ac057168b21bbd275f415f1b7d15b7a7c4
iGoR-KoV <ikigko@gmail.com>
2019-08-15 11:14:38 +0300

    task0618

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0618/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task06.task0618;
+
+/* 
+KissMyShinyMetalAss
+*/
+
+import java.awt.*;
+
+public class Solution {
+    public static class KissMyShinyMetalAss {
+
+    }
+
+    public static void main(String[] args) {
+        KissMyShinyMetalAss robot = new KissMyShinyMetalAss();
+        System.out.println(robot);
+    }
+}
