331d33cb0a024f4833349bb05e23e2e9a82e673e
iGoR-KoV <ikigko@gmail.com>
2019-08-26 20:51:00 +0300

    task0807

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0807/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task08.task0807;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+LinkedList и ArrayList
+*/
+
+public class Solution {
+    public static Object createArrayList() {
+        List<Object> arrList = new ArrayList<>();
+        return arrList;
+    }
+
+    public static Object createLinkedList() {
+        List<Object> linkList = new LinkedList<>();
+        return linkList;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
