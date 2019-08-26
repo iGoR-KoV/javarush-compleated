d8042614e5d1589c44d72c350bbbbb2b8aa57b60
iGoR-KoV <ikigko@gmail.com>
2019-08-27 00:42:20 +0300

    task0811

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0811/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task08.task0811;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Квартет «Методы»
+*/
+
+public class Solution {
+    public static List getListForGet() {
+        return new ArrayList<List>();
+    }
+
+    public static List getListForSet() {
+        return new ArrayList<List>();
+    }
+
+    public static List getListForAddOrInsert() {
+        return new LinkedList<List>();
+
+    }
+
+    public static List getListForRemove() {
+        return new LinkedList<List>();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
