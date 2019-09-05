d1b441268cbaba149ca05885d01d813a7af72d18
iGoR-KoV <ikigko@gmail.com>
2019-09-05 10:48:23 +0300

    task0829

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0829/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task08.task0829;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/* 
+Модернизация ПО
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        HashMap<String, String> addresses = new HashMap<>();
+        while (true) {
+            String city = reader.readLine();
+            if (city.isEmpty()) break;
+            String family = reader.readLine();
+            addresses.put(city, family);
+        }
+        String findCity = reader.readLine();
+        System.out.println(addresses.get(findCity));
+
+
+
+
+
+//        List<String> list = new ArrayList<>();
+//        while (true) {
+//            String family = reader.readLine();
+//            if (family.isEmpty()) {
+//                break;
+//            }
+//
+//            list.add(family);
+//        }
+//
+//        // Read the house number
+//        int houseNumber = Integer.parseInt(reader.readLine());
+//
+//        if (0 <= houseNumber && houseNumber < list.size()) {
+//            String familyName = list.get(houseNumber);
+//            System.out.println(familyName);
+//        }
+    }
+}
