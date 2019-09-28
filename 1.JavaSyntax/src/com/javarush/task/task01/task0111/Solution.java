3425ad04e6bab01ae395bd0d2dd25f5403a80f0e
iGoR-KoV <ikigko@gmail.com>
2019-09-28 13:44:42 +0300

    task0111

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0111/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task01.task0111;
+
+/* 
+Одной переменной недостаточно
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+       int a = 3;
+       int b = 4;
+       String s = "alesha";
+    }
+}
