d88f83b29ec4e341d429c674f1c6264329c4134b
iGoR-KoV <ikigko@gmail.com>
2019-10-01 22:49:46 +0300

    task0121

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0121/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task01.task0121;
+
+/* 
+Контракт
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+      String s = "Я согласен на зарплату";
+      System.out.println("Меня зовут Амиго.");
+      System.out.println("");
+      System.out.println(s+" "+ "$800/месяц в первый год.");
+      System.out.println(s+ " "+ "$1500/месяц во второй год.");
+      System.out.println(s+" " + "$2200/месяц в третий год.");
+      System.out.println(s+ " " + "$2700/месяц в четвертый год.");
+      System.out.println(s+ " " + "$3200/месяц в пятый год.");
+      System.out.println("");
+      System.out.println("Поцелуй мой блестящий металлический зад!");
+      //напишите тут ваш код
+    }
+}
