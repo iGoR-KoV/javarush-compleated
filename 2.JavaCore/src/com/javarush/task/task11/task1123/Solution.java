0434cd195fa2ecbe399df8aab7df7f622cb5d412
iGoR-KoV <ikigko@gmail.com>
2019-10-02 05:45:32 +0300

    task1123

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1123/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task11.task1123;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+        Pair<Integer, Integer> result = getMinimumAndMaximum(data);
+
+        System.out.println("The minimum is " + result.x);
+        System.out.println("The maximum is " + result.y);
+    }
+
+    public static Pair<Integer, Integer> getMinimumAndMaximum(int[] inputArray) {
+        if (inputArray == null || inputArray.length == 0) {
+            return new Pair<Integer, Integer>(null, null);
+        }
+
+        int [] data2 = Arrays.copyOf(inputArray, inputArray.length);
+        Arrays.sort(data2);
+
+        int min = 0;
+        int max = 0;
+
+        for (int i = 0; i < data2.length; i++) {
+             min = data2[0];
+             max = data2[data2.length - 1];
+
+        }
+
+
+
+        return new Pair<Integer, Integer>(min, max);
+    }
+
+    public static class Pair<X, Y> {
+        public X x;
+        public Y y;
+
+        public Pair(X x, Y y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
