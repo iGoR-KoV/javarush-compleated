81675adca7aad5c898220c8bda36233117d46954
iGoR-KoV <ikigko@gmail.com>
2019-10-06 15:06:23 +0300

    task1208

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int a) {}
+    public static void print(String b) {}
+    public static void print(short c) {}
+    public static void print(int a, String b) {}
+    public static void print(Integer d) {}
+}
