3a396e1d1fd90716c058671b184877fed3337b6c
iGoR-KoV <ikigko@gmail.com>
2019-10-07 22:49:18 +0300

    task1217

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        void flyable();
+    }
+    public interface CanRun {
+        void runable();
+    }
+    public interface CanSwim {
+        void swimable();
+    }
+
+}
