8d162d731b2110a3cdc50fe504b8a3b942c5c441
iGoR-KoV <ikigko@gmail.com>
2019-10-13 14:28:14 +0300

    task1309

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1309/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task13.task1309;
+
+/* 
+Всё, что движется
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface CanMove {
+        Double speed();
+    }
+    public interface CanFly extends CanMove {
+
+        default Double speed(CanFly fly) {
+            return null;
+        }
+    }
+}
