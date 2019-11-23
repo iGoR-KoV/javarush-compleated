f2e2da494696a45db0b450f382e78dd41ba4ebe6
iGoR-KoV <ikigko@gmail.com>
2019-11-24 00:46:47 +0200

    MinesweeperGame2/16

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -3,4 +3,13 @@ package com.javarush.games.minesweeper;
 import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game {
+    private static final int SIDE = 9;
+    @Override
+    public void setScreenSize(int width, int height) {
+    }
+
+    @Override
+    public void initialize() {
+        setScreenSize(SIDE, SIDE);
+    }
 }
