48265159b7f50ccd2c771acecbdd4b1c3219705e
iGoR-KoV <ikigko@gmail.com>
2019-11-25 00:29:09 +0200

    MinesweeperGame5/16

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -3,31 +3,29 @@ package com.javarush.games.minesweeper;
 import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game {
-    private static final int SIDE = 9;
-    private GameObject[][] gameField = new GameObject[SIDE][SIDE];
-
-    @Override
-    public void setScreenSize(int width, int height) {
-    }
+    private  static final int SIDE = 9;
+    private  int  countMinesOnField = 0;
+    private boolean mine;
+    private GameObject[][]  gameField = new GameObject[SIDE][SIDE];
 
     @Override
     public void initialize() {
-        super.initialize();
-        super.setScreenSize(SIDE, SIDE);
+        setScreenSize(SIDE, SIDE);
         createGame();
     }
 
-    @Override
-    public void setCellColor(int x, int y, Color color) {
-        super.setCellColor(x, y, Color.ORANGE);
-    }
-
     private void createGame() {
+        for(int x = 0; x < gameField.length; x++) {
+            for (int y = 0; y < gameField[0].length; y++) {
 
-        for (int i = 0; i < SIDE; i++) {
-            for (int j = 0; j < SIDE; j++) {
-                gameField[j][i] = new GameObject(i, j);
-                setCellColor(i, j, Color.ORANGE);
+                if (getRandomNumber(10) == 0) {
+                    mine = true;
+                    countMinesOnField++;
+                } else {
+                    mine = false;
+                }
+                gameField[y][x] = new GameObject(x, y, mine);
+                setCellColor(x, y, Color.ORANGE);
             }
         }
     }
