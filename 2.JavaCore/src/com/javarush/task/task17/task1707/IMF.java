472fadf705a13a962d01af4ac225918bc9ca504b
iGoR-KoV <ikigko@gmail.com>
2019-12-05 22:53:02 +0200

    task1707

---
+++ 2.JavaCore/src/com/javarush/task/task17/task1707/IMF.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task17.task1707;
+
+
+public class IMF {
+
+    private static IMF imf;
+
+    public static IMF getFund() {
+        synchronized (IMF.class) {
+            if (null == imf) {
+                imf = new IMF();
+            }
+        }
+        return imf;
+    }
+
+    private IMF() {
+    }
+}
