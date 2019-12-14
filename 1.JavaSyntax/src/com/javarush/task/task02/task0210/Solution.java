1641a51102722146f2d208f3a068e3d69e7c9ef9
iGoR-KoV <ikigko@gmail.com>
2019-12-14 23:53:29 +0200

    task0210

---
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0210/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task02.task0210;
+
+/* 
+Необъективная реальность
+*/
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Будешь плохо кодить,");
+        System.out.println("придет Java и съест твою память");//напишите тут ваш код
+    }
+}
