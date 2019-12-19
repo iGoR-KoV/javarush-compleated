f927449f3a06cb804440beef0451e07c095222d8
iGoR-KoV <ikigko@gmail.com>
2019-12-19 23:41:18 +0200

    task0316

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0316/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task03.task0316;
+
+/* 
+Экранирование символов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+       System.out.println("It's Windows path: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"\nIt's Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk1.7.0\\\\bin\\\"");//напишите тут ваш код
+    }
+}
