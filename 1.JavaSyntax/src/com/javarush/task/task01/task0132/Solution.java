c25b82197b8e12a4d55cbe01bf1b777578434a11
iGoR-KoV <ikigko@gmail.com>
2020-01-14 21:10:00 +0200

    task1811

--- 1.JavaSyntax/src/com/javarush/task/task01/task0132/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0132/Solution.java
@@ -13,8 +13,7 @@ public class Solution {
         int num1 = number % 10;
         int num2 = number / 10 % 10;
         int num3 = number /100 % 10;
-        return num1 + num2 + num3;
-        //напишите тут ваш код
+        return num1 + num2 + num3;//напишите тут ваш код
     }
 
 }
